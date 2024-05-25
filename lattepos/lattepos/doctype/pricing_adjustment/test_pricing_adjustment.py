61c2b678944acac15067da67c327ec4dc6c6e146
nithyanandhasubramanya <nithyanandha.subramanya@gmail.com>
2024-05-25 19:54:34 +0000

    Added Payment,Payment processor,Pricing Adjustment Doctypes Moved Tips ans Final Price into payments. Added Discounts and Surcharge to Order.

---
+++ lattepos/lattepos/doctype/pricing_adjustment/test_pricing_adjustment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, nammamane software group and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPricingAdjustment(FrappeTestCase):
+	pass
